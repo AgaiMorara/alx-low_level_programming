5323c76c40cab055ccaed9f573afb27505d450bc
AgaiMorara <corneliusmorara@gmail.com>
2022-06-30 19:35:53 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
-# include <stdio.h>
+#include <stdio.h>
+/*everything happens here*/
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-           return(0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return(0);
 }
