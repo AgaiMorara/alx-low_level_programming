b7fb08b9478e37711e4bf9c7e3f465e9656494ef
AgaiMorara <corneliusmorara@gmail.com>
2022-07-01 18:25:48 -0700

    simulate

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,19 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+
+/**
+ *main  -> simulates random numbers and tags if they are positive,negative or zero
+ *Return :0 tells compiler, program ran successfully
+ */
 int main(void)
 {
-  int n;
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  /* your code goes there */
-  if (n < 0)
-    printf("%i is negative\n",n);
-  else if (n == 0)
-    printf("%i is zero\n",n);
-  else
-    printf("%i is positive\n",n);
-  return (0);
+int n;
+  
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n < 0)
+  printf("%i is negative\n", n);
+else if (n == 0)
+  printf("%i is zero\n", n);
+else
+  printf("%i is positive\n", n);
+return (0);
 }
