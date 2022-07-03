5146c9f4caa030c6eb6394f57f57e628205c4cba
AgaiMorara <corneliusmorara@gmail.com>
2022-07-03 07:24:55 -0700

    ascii comma and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - program that prints numbers base 10
+ *program implements ascii and putchar
+ *Return: int 0 if successful
+ */
+
+int main(void)
+{
+	int N;
+
+	for (N = 48; N < 58; N++)
+	{
+		putchar(N);
+		putchar(44);
+		putchar(32);
+	}
+	putchar(10);
+	return (0);
+}
