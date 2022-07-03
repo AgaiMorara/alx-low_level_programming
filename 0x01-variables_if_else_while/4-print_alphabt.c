144e0080464047d47a31ce589712aef8b4c8a7a5
AgaiMorara <corneliusmorara@gmail.com>
2022-07-03 06:03:26 -0700

    alphanumeric with logicals

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - program that prints the alphabent in lowercase
+ *program should only use putchar function
+ *Return: int 0 if successful
+ */
+
+int main(void)
+{
+	int N;
+
+	for (N = 97; N <= 122; N++)
+	{
+		if (N != 'e' && N != 'q')
+		putchar(N);
+	}
+	putchar(10);
+	return (0);
+}
