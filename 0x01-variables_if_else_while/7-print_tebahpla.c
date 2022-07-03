81f11490165bd51c7a56b6a851e53baf57b5084c
AgaiMorara <corneliusmorara@gmail.com>
2022-07-03 06:40:11 -0700

    ascii reverse lower alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - program that prints lower alphabet in reverse
+ *program should only use putchar function
+ *Return: int 0 if successful
+ */
+
+int main(void)
+{
+	int N;
+
+	for (N = 122; N <= 97; N--)
+	{
+		putchar(N);
+	}
+	putchar(10);
+	return (0);
+}
