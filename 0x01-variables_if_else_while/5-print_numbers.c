bec627533c44de73fbdd9e29b326454734d98da0
AgaiMorara <corneliusmorara@gmail.com>
2022-07-03 06:21:17 -0700

    base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - program that prints numbers base 10
+ *program implements the basic for loop
+ *Return: int 0 if successful
+ */
+
+int main(void)
+{
+	int N;
+
+	for (N = 0; N < 10; N++)
+	{
+		printf("%i\n",N);
+	}
+	return (0);
+}
