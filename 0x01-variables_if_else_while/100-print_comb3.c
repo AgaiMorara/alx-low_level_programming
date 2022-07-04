b405d1991192d02f52cfbe2109823ccb36dea8aa
AgaiMorara <corneliusmorara@gmail.com>
2022-07-04 09:32:51 -0700

    Nested , for if

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ *main - program that prints numbers base 10
+ *program implements ascii and putchar
+ *Return: int 0 if successful
+ */
+
+int main(void)
+{
+	int N;
+
+	int P;
+
+	for (N = 48; N < 58; N++)
+	{
+		for (P = 48; N < 58; N++)
+		{
+			if (P > N)
+			{
+				putchar(N);
+				putchar(P);
+				if (P == 57)
+					break;
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
