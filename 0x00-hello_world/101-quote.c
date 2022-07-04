600c4bfab23ecda027dc8a9367b5c7f9a4e64758
AgaiMorara <corneliusmorara@gmail.com>
2022-07-04 08:17:30 -0700

    masm =intel"

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - program ues prints to the standard error
+ *without using printf, puts or any of their family functions
+ *Return: 1 program finished successfully
+ */
+
+int main()
+{
+	fwrite("and that piece of art is useful\" - Dora korpar, 2015-10-19\n",59,stderr);
+return (1);
+}
---
+++ 0x00-hello_world/101-quote.c~
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - program uses printf to print a statement
+ *escapes at the beginning
+ *Return: 0 program finished successfully
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
