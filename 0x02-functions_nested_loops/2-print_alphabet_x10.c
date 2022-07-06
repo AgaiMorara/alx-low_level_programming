b51ec65bc6f74f7d899226e3c3470b94474688b2
AgaiMorara <corneliusmorara@gmail.com>
2022-07-05 19:33:39 -0700

    print alphabet severally

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - program that prints the alphabent in lowercase
+ *program should only use _putchar function from our lib
+ *Return: int 0 if successful
+ */
+
+void print_alphabet_x10(void)
+{
+	char N;
+	int i;
+	for (i = 1; i <= 10; i++)
+	{
+		for (N = 'a'; N <= 'z'; N++)
+		{
+			_putchar(N);
+		}
+		_putchar('\n');
+	}
+
+}
