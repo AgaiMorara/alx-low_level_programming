c20fa84a4e8c3f04780b8305c6d967b30aba8abb
AgaiMorara <corneliusmorara@gmail.com>
2022-07-10 01:10:46 -0700

    whitespaces

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,18 +10,22 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 101 ; i++)
+	for (i = 1; i <= 100 ; i++)
 	{
-		if (i % 5 == 0 && i % 3 == 0)
+		if (i % 15 == 0)
 			printf("FizzBuzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+		{
+			if (i == 100)
+				printf("Buzz");
+			else
+				printf("Buzz ");
+		}
 		else
 			printf("%i ", i);
 	}
-	putchar(8);
 	putchar(10);
 	return (0);
 }
