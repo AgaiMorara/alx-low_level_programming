a52a1abb2930c5e4f12746c8810875bf9a47a676
AgaiMorara <corneliusmorara@gmail.com>
2022-07-11 09:50:20 -0700

    equating variables, different addresses

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *swap_int- swaps values of two integers variables
+ *@a:the first variable
+ *@b:the second variable
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *b;
+	*b = *a;
+
+	*a = temp;
+}
