b2c8b128e0dd4b8dde3063a5dc5fd57d605d2a21
AgaiMorara <corneliusmorara@gmail.com>
2022-07-11 09:17:40 -0700

    dereferencing function

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  *reset_to_98 - resets the value of a variable to 98
  *takes a pointer to an integer as its parameter
- *@ *n - pointer to an integer
+ *@ *n: pointer to an integer
  */
 void reset_to_98(int *n)
 {
