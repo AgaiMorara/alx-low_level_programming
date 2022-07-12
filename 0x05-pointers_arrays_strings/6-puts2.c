47b38bd7769c8077ed3c8b30e40f927accbc4722
AgaiMorara <corneliusmorara@gmail.com>
2022-07-12 14:11:59 -0700

    fixed betty

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,20 @@
 #include "main.h"
+#include <string.h>
 
 /**
  *puts2 - prints every second character.. (jumps characters in odd number spots
- *
+ *@str: string whose content should be printed evenly
  */
 
 void puts2(char *str)
 {
-	int strlen = 0;
+	int i = 0;
+	int j = strlen(str);
 
-	while (str[strlen] != '\0')
+	while (i < j)
 	{
-		_putchar(str[strlen]);
-		strlen += 2;
+		_putchar(str[i]);
+		i += 2;
 	}
 	_putchar(10);
 }
