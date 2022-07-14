74cbc72355deba393b8ac222399e24aa85abc856
AgaiMorara <corneliusmorara@gmail.com>
2022-07-14 12:41:34 -0700

    string compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strcmp- compares two strings
+ *
+ *Return: integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	int indicator = 0;
+
+	while (indicator == 0)
+	{
+		if (s1[i] > s2[i])
+			indicator = 1;
+		else if (s1[i] < s2[i])
+			indicator = -1;
+		if (s1[i] == '\0')
+			break;
+		i++;
+	}
+	return (indicator);
+}
