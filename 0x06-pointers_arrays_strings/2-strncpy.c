4969c1688c9bb374d2031182eb79645fd5930f48
AgaiMorara <corneliusmorara@gmail.com>
2022-07-14 07:52:27 -0700

    no stars after First

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -29,7 +29,10 @@ char *_strncpy(char *dest, char *src, int n)
 		src++;
 		i++;
 	}
-	for ( ; i < n; i++)
-		dest[i] = '\0';
-	return (dest_ptr);
+	*dest = '\0';
+	/**
+	 *for ( ; i < n; i++)
+	 *dest[i] = '\0';
+	 */
+		return (dest_ptr);
 }
