d205942f8a344869ed38199bc58613fd3b6fb478
AgaiMorara <corneliusmorara@gmail.com>
2022-07-15 04:46:46 -0700

    alpha flag

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* _isalpha - flag  function that identifies any alphabet character
+*@c: character to be identified
+*Return: 1 if alphabet 0 otherwise
+*/
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || ( c >= 'A' && c <= 'Z'));
+}
