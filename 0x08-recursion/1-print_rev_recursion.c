43b3fb5c4cf11e82647728773dacd556bf7f78ee
AgaiMorara <corneliusmorara@gmail.com>
2022-07-20 14:58:34 -0700

    direction always opposite

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - recursive form of puts
+ *@s:pointer to  string to be printed
+ *
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+		_print_rev_recursion(s + 1);
+	else
+	{
+		return;
+	}
+	_putchar(*s);
+}
