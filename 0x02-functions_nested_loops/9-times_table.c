e56fb8948e004b922eabba4163e133fcf1c8acb2
AgaiMorara <corneliusmorara@gmail.com>
2022-07-20 01:47:07 -0700

    multiplication table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - prints the multication table for single digits numbers
+ *
+ */
+
+void times_table(void)
+{
+        int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		j = 0, k = 0;
+
+		while (k < 10)
+		{
+			if (j >= 10 && k > 1)
+			{
+				_putchar((j / 10) + 48);
+			}
+			else if ( k > 1)
+				_putchar(' ');
+
+			_putchar((j % 10) + 48);
+			if (k == 9)
+			{
+				break;
+			}
+			_putchar(',');
+			_putchar(' ');
+			j += i;
+			k++;
+		}
+	_putchar(10);
+	}
+}
