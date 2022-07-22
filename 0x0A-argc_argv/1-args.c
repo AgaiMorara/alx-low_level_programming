41841ba49aaf3d4d0675f093100ef9c64dbf6e4f
AgaiMorara <corneliusmorara@gmail.com>
2022-07-22 12:55:21 -0700

    number of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *@argc: size of arguments
+ *@argv: a pointer to a vector of pointers to the string arguments
+ *Return: 0 - success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
