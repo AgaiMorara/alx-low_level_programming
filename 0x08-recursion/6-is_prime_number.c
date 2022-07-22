eaef9475c849ada171112efee23d74fc986f28f0
AgaiMorara <corneliusmorara@gmail.com>
2022-07-21 19:33:04 -0700

    helper function necesary

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,16 +1,32 @@
 #include "main.h"
 
 /**
- *is_prime_number - checks wether n is prime
- *@n:  input for the prime function
- *
- *Return: 1 if is a prime number, 0 otherwise
+ *prime_find - helper function to accomodate variable
+ *@n: input for root function
+ *@r: constant aid variable checker
+ *Return: 1 if n is prime 0 otherwise
  */
+int prime_find(int n, int r)
+{
+
+	if (r == 1 && n >= 2)
+		return (1);
+	else if (n < 2)
+		return (0);
+	else if (n % r == 0)
+		return (0);
+	return (prime_find(n, r - 1));
+}
+
+/**
+ *is_prime_number - function to return primenumber
+ *@n: input
+ *Return: output of helper function
+ */
+
+
+
 int is_prime_number(int n)
 {
-	if (n < 2)
-		return (0);
-	else if (n % x == 0)
-		return (1);
-	return (n ** (1 / 2));
+	return (prime_find(n, n / 2));
 }
