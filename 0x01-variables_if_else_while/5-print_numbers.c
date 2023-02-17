0ecbc08a5fdd251258efd6f15ae6a534a4d45c14
AgaiMorara <corneliusmorara@gmail.com>
2023-02-17 13:44:29 -0800

    reused same code from no 2-7

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+
+/**
+ *main - entry point to alphabet program
+ *
+ *Return: 0 success
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(48 + i);
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
