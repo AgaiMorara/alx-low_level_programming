499672b09945e8f0ca235e1034e295c4c93626b9
AgaiMorara <corneliusmorara@gmail.com>
2023-02-26 11:54:49 -0800

    first custom primitive

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_isupper - checks for uppercase characters
+ *@c: character to be checked
+ *Return: 1 if c is uppercase 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+		return (1);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+Reconstruct some natives using primitives. More activity is more progress towards
+maing a good software Engineer.
