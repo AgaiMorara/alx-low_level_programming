cb3a01c0b51dfb6fc1193372c4b2b6b7bcff6059
AgaiMorara <corneliusmorara@gmail.com>
2023-02-27 17:29:07 -0800

    odd ended string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,10 +7,16 @@
 
 void puts2(char *str)
 {
+	int i = 0, j = 0;
 
-	while (*str != '\0')
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+	while (*str != '\0' && j < i)
 	{
 		_putchar(*str);
+		j += 2;
 		str += 2;
 	}
 	_putchar(10);
