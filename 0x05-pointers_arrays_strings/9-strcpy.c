52d454154d2521d3b6751d5039442935641302f0
AgaiMorara <corneliusmorara@gmail.com>
2023-02-28 13:32:41 -0800

    two birds one stone

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,1 +1,17 @@
 # include "main.h"
+
+/**
+ *_strcpy - copies string to buffer pointed by second string
+ *@dest: destination
+ *@src: source
+ *Return:dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *ans = dest;
+
+	while ((*dest++ = *src++))
+		;
+	return (ans);
+}
