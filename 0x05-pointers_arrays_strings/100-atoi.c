d6df7dc4ce8a75d2796d621f3eff22ed291165f1
AgaiMorara <corneliusmorara@gmail.com>
2023-03-13 12:56:10 -0700

    show up + solve

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,1 +1,31 @@
-# include "main.h"
+#include "main.h"
+
+/**
+ *_atoi - converts a string to an int
+ *@s: The string
+ *Return: Integer from the string
+ */
+
+int _atoi(char *s)
+{
+	int i = 0, j = 0, k = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) >= '0'  && *(s + i) <= '9')
+		{
+			if (j == 0)
+				j += *(s + i) - '0';
+			else
+				j = j * 10 + (*(s + i) - '0');
+		}
+		if (j == 0 && *(s + i) == '-')
+			k += 1;
+		i++;
+		if (j > 0 && *(s + i) == ' ')
+			return (j);
+	}
+	if (k % 2 == 1)
+		j *= -1;
+	return (j);
+}
