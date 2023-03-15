660b36f763d6cfacd65f9fdcdfa23fcfc0716a28
AgaiMorara <corneliusmorara@gmail.com>
2023-03-14 18:08:00 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,13 @@ char *str_concat(char *s1, char *s2)
 	char *conc, *ans;
 	int i = 0, j = 0;
 
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	else if (s1 == NULL)
+		return (s2);
+	else if (s2 == NULL)
+		return (s1);
+
 	while (*(s1 + i) != '\0')
 		i++;
 	while (*(s2 + j) != '\0')
