98d3b9ba2a668ebc0e2676a48ca8e0a4b7c1c0a1
AgaiMorara <corneliusmorara@gmail.com>
2023-03-14 23:55:06 -0700

    knacked it

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -4,12 +4,13 @@
  *argstostr - concatenates all arguments of a program
  *@ac: argument count
  *@av: argument vector
+ *Return: concated arguments
  */
 
-char *argstostr(int ac, char ** av)
+char *argstostr(int ac, char **av)
 {
 	char *ans;
-	int i = 0, j = 0, k = 50;
+	int i = 0, j = 0, k = 50, l = 0;
 
 	if (ac == 0 || !av)
 		return (NULL);
@@ -20,19 +21,20 @@ char *argstostr(int ac, char ** av)
 		while (ac--)
 		{
 			i = 0;
-			while (*(av[i]) != '\0' && ans)
+			while (av[l][i] != '\0' && ans)
 			{
 				if (j >= k)
 				{
 					k += 50;
 					ans = realloc(ans, k);
 				}
-				*(ans + j) = *av[i];
+				*(ans + j) = av[l][i];
 				j++;
 				i++;
 			}
 			*(ans + j) = '\n';
 			j++;
+			l++;
 		}
 		*(ans + j) = '\0';
 	}
