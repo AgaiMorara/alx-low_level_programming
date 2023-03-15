cc779b7ec2ab63cb9d802fe9a6a43acf2f3acdb1
AgaiMorara <corneliusmorara@gmail.com>
2023-03-14 18:19:43 -0700

    Null strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,16 +12,9 @@ char *str_concat(char *s1, char *s2)
 	char *conc, *ans;
 	int i = 0, j = 0;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
-	else if (s1 == NULL)
-		return (s2);
-	else if (s2 == NULL)
-		return (s1);
-
-	while (*(s1 + i) != '\0')
+	while (s1 && *(s1 + i) != '\0')
 		i++;
-	while (*(s2 + j) != '\0')
+	while (s2 && *(s2 + j) != '\0')
 		j++;
 
 	conc = malloc(sizeof(char) * (i + j) + 1);
@@ -29,11 +22,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (conc != NULL)
 	{
-		while (i--)
+		while (i-- && s1)
 			*conc++ = *s1++;
-		while (j--)
+		while (j-- && s2)
 			*conc++ = *s2++;
-		*conc = '\0';
 	}
+	*conc = '\0';
 	return (ans);
 }
