3f0d8c0fd22d73e0931e197be6f0d74696321bc0
AgaiMorara <corneliusmorara@gmail.com>
2023-03-22 17:41:16 -0700

    betty errors

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,23 +1,23 @@
 #include "function_pointers.h"
 
 /**
-(int_index - searches for an integer
-*@array: elements in an array from among which to search
-*@size: size of the array
-*@cmp: function pointer to aid in the search
-*/
+ *int_index - searches for an integer
+ *@array: elements in an array from among which to search
+ *@size: size of the array
+ *@cmp: function pointer to aid in the search
+ *Return: integer index
+ */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0, h = 0;
-	if (size <= 0)
-		return (-1);
-	while (i < size && h != 1)
+
+	while (i < size && h == 0)
 	{
 		h = cmp(array[i]);
 		i++;
 	}
-	if (i == size)
+	if (i == size || size <= 0)
 		return (-1);
 
 	return (i - 1);
