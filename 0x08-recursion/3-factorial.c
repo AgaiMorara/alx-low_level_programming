2df26991a702a467f6b7da222770044ead0deb59
AgaiMorara <corneliusmorara@gmail.com>
2023-06-07 23:50:55 +0300

    betty corrections && git push

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -5,11 +5,11 @@
  *Return: factorial
  */
 
-int factorial (int n)
+int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
-	else if ( n == 0 || n == 1)
+	else if (n == 0 || n == 1)
 		return (1);
-	return ( n * factorial (n -1));
+	return (n * factorial(n - 1));
 }
