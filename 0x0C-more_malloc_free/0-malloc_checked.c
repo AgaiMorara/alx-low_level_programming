e3226103b4ff1ef3037a40d0033b6093e9e34236
AgaiMorara <corneliusmorara@gmail.com>
2023-06-16 00:22:20 +0300

    exit(3)

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *malloc_checked - allocates memory using malloc
+ *@b: amount of memory to return
+ *Returns: the memory address
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *check;
+	check = malloc(b);
+
+	if (!check)
+		exit(98);
+	return (check);
+}
