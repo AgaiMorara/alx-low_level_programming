b40889cc5e7a94fe7e5be1c3a585abbc05ca1208
AgaiMorara <corneliusmorara@gmail.com>
2023-07-02 13:30:31 +0300

    delta top

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,11 +9,14 @@
 
 void print_dog(struct dog *d)
 {
-	printf("Name : %s\n", !(d->name) ? "(nil)" : d->name);
-	printf("Age: ");
-	if (d->age)
-		printf("%f\n", d->age);
-	else
-		printf("%d\n", 0);
-	printf("Owner: %s\n", !(d->owner) ? "(nil)" : d->owner);
+	if(d)
+	{
+		printf("Name: %s\n", !(d->name) ? "(nil)" : d->name);
+		printf("Age: ");
+		if (d->age)
+			printf("%f\n", d->age);
+		else
+			printf("%d\n", 0);
+		printf("Owner: %s\n", !(d->owner) ? "(nil)" : d->owner);
+	}
 }
