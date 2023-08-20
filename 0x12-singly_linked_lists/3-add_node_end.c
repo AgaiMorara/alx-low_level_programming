8f8ae6d3481f89605d662cf43473d890712c08b9
AgaiMorara <corneliusmorara@gmail.com>
2023-08-20 13:08:10 +0300

    head *

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,21 +13,24 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *tracker = *head;
 	list_t *end = malloc(sizeof(list_t));
 
-	if (end == NULL)
+	if (!end)
 	{
 		free(end);
 		return (NULL);
 	}
 	end->str = strdup(str);
 	if (!end->str)
+	{
 		free(end->str);
+		return (NULL);
+	}
 	end->len = strlen(end->str);
 	end->next = NULL;
 
 	if (tracker == NULL)
 	{
 		*head = end;
-		return (head);
+		return (*head);
 	}
 
 	while (tracker->next != NULL)
