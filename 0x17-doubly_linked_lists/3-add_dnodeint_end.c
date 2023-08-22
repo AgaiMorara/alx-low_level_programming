ec8af300575b7f48bf259b7c33a863aa42ffb89a
AgaiMorara <corneliusmorara@gmail.com>
2023-08-22 08:44:16 +0300

    node at the end

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ *add_dnodeint_end - add a new node at the end of dlistint_t
+ *@head: list
+ *@n: member of node
+ *Return: address of the new element
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *end = malloc(sizeof(dlistint_t));
+	dlistint_t *traverser;
+
+	if (!end)
+	{
+		return (NULL);
+	}
+	(*end).n = n;
+	(*end).next = NULL;
+	if (*head == NULL)
+	{
+		(*end).prev = NULL;
+		*head = end;
+		return (*head);
+	}
+	traverser = *head;
+	while (traverser->next != NULL)
+		traverser = traverser->next;
+	traverser->next = end;
+	traverser->prev = traverser;
+	return (end);
+}
