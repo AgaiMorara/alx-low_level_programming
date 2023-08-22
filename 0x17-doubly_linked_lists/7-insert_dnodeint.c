cddc4373b3dad6c50bd95dfac77bdfa774f62893
AgaiMorara <corneliusmorara@gmail.com>
2023-08-22 21:00:30 +0300

    insert dnode

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,26 +9,43 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *trak = *h;
-	dlistint_t *node;
+    dlistint_t *trak = *h;
+    dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
-	while (trak != NULL && idx > 0)
-	{
-		trak = trak->next;
-		idx--;
-	}
-	if (trak == NULL || idx > 0)
-		return (NULL);
-	node = malloc(sizeof(dlistint_t));
+    if (idx == 0)
+    {
 
-	if (!node)
-		return (NULL);
-	(*node).n = n;
-	(*node).next = trak;
-	if (trak->prev)
-		(*node).prev = trak->prev;
-	else
-		(*node).prev = NULL;
-	trak = node;
-	return (trak);
+        if (!new_node)
+            return NULL;
+
+        new_node->n = n;
+        new_node->prev = NULL;
+        new_node->next = trak;
+
+        if (trak)
+            trak->prev = new_node;
+
+        *h = new_node;
+        return new_node;
+    }
+
+    while (trak != NULL && idx > 1) {
+        trak = trak->next;
+        idx--;
+    }
+
+    if (trak == NULL)
+        return NULL;
+
+    if (!new_node)
+        return NULL;
+
+    new_node->n = n;
+    new_node->prev = trak;
+    new_node->next = trak->next;
+
+    if (trak->next)
+        trak->next->prev = new_node;
+    trak->next = new_node;
+    return new_node;
 }
