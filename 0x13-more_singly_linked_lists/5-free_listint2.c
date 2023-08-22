7faeb243ceef779d07080dafa6a22b4780a7ddcd
AgaiMorara <corneliusmorara@gmail.com>
2023-08-22 11:34:00 +0300

    free list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -21,6 +21,8 @@ void free_listint(listint_t *head)
  */
 void free_listint2(listint_t **head)
 {
+	if (head == NULL || *head == NULL)
+		return;
 	listint_t *current = *head;
 
 	free_listint(current);
