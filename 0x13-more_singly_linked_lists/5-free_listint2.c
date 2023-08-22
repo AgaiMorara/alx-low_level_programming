d08e61d0cd76275eca4e10cff24119a514f4f441
AgaiMorara <corneliusmorara@gmail.com>
2023-08-22 11:31:23 +0300

    free list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ *free_listint - frees a singly linked list
+ *@head: entry point to list
+ */
+void free_listint(listint_t *head);
+
+void free_listint(listint_t *head)
+{
+	if (head)
+	{
+		if (head->next != NULL)
+			free_listint(head->next);
+		free(head);
+	}
+}
+
+/**
+ *free_listint2 - container to ensure *head is null
+ *@head: head of linked list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+
+	free_listint(current);
+	*head = NULL;
+}
