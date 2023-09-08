b46d253199c543963fdf4392a7c616acb6f570bc
AgaiMorara <corneliusmorara@gmail.com>
2023-09-09 00:09:20 +0300

    structure

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+/**
+ *hash_table_create - creates a hash table
+ *@size: the size of the array
+ *Return: pointer to hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ans;
+	unsigned long int i;
+
+	ans = malloc(sizeof(hash_table_t));
+
+	if (!ans || size <= 0)
+		return (NULL);
+	ans->size = size;
+	ans->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (!ans->array)
+	{
+		free (ans);
+		return (NULL);
+	}
+
+	while (i < size)
+	{
+		ans->array[i] = NULL;
+		i++;
+	}
+
+	return (ans);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,4 @@
+0x1A. C - Hash tables
+
+We implement a hash table in C... for this project.
+Choosing a good hash function... Hashing techniques
