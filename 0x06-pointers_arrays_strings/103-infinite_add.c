78a36c2f67c9265e6591421fb4223620ee2239e4
AgaiMorara <corneliusmorara@gmail.com>
2023-09-09 21:31:34 +0300

    variable tracking

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,57 +1,59 @@
 #include "main.h"
+#include <string.h>
+
 /**
- *reverse - reverses a string
- *@n: length of the string
+ * reverse - reverses a character string
+ * @s: the string to be reversed
  */
-
-void reverse(int *a, int n)
+void reverse(char *s)
 {
-	int i, j, temp;
+	int length = strlen(s);
+	int i, j;
+	char temp;
 
-	for (i = 0, j = n - 1 ; i < n / 2 ; i++, j--)
+	for (i = 0, j = length - 1; i < j; i++, j--)
 	{
-		temp = a[i];
-		a[i] = a[j];
-		a[j] = temp;
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
 	}
 }
 
 /**
- * infinite_add - add integers provided as strings and return them as string
- *@n1: pointer to string with first number
- *@n2: pointer to string with second number
- *@r: buffer to store the result
- *@size_r: buffer size
+ * infinite_add - add integers provided as strings and return them as a string
+ * @n1: pointer to string with the first number
+ * @n2: pointer to string with the second number
+ * @r: buffer to store the result
+ * @size_r: buffer size
+ * Return: pointer to the result string
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	char *ans = r;
 	int i = 0, j = 0, carry = 0, num1, num2, sum;
 
-	while (*n1++ != '\0', i++);
-	while(*n2++ != '\0', j++);
+	while (n1[i] != '\0')
+		i++;
+	while (n2[j] != '\0')
+		j++;
+	if (i > size_r - 1 || j > size_r - 1)
+		return (NULL);
+	i--;
+	j--;
 
-	if (i > size_r - 1)
-		return;
-	else
-		num1 == n1[i] + '0';
-	if (j > size_r - 1)
-		return;
-	else
-		num2 == n2[j] + '0';
-
-	while (i || j || carry)
+	while (i >= 0 || j >= 0 || carry > 0)
 	{
-		if (i == 0)
-			num1 = 0;
-		if (j == 0)
-			num2 == 0;
+		num1 = (i >= 0) ? n1[i] - '0' : 0;
+		num2 = (j >= 0) ? n2[j] - '0' : 0;
 		sum = num1 + num2 + carry;
 		carry = sum / 10;
+
 		*r = sum % 10 + '0';
 		i--, j--, r++;
-    }
-	reverse(ans, size_r);
+	}
+
+	*r = '\0';
+	reverse(ans);
+
 	return (ans);
 }
